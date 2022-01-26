5caa611866b5dbbb2483625efe06179101bcac02
lakshanlach <97869720+lakshanlach@users.noreply.github.com>
2022-01-26 21:19:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    Canvas = createCanvas(1024 , 1024)
+    Canvas.center()
+}
+
+function draw() {
+
+}
+
+function takesnapshot() {
+    save("mustache or lipstick.png")
+}
